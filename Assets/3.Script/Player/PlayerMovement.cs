6d3d280c83efa9525a52ce7ed32bcd2c1994b489
HaloTwo <lsm6265@naver.com>
2023-06-22 18:00:24 +0900

    Update

--- Assets/3.Script/Player/PlayerMovement.cs
+++ Assets/3.Script/Player/PlayerMovement.cs
@@ -6,17 +6,38 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private float defaultSpeed = 900;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] private float walkSpeed = 400;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private PlayerControlsButton inputActions;
+    private CharacterController controller;
     private Animator animator;
 
+
     private Vector3 moveDirection;
-    [SerializeField] private float defaultSpeed = 400f;
-    [SerializeField] private float currentSpeed;
-    [SerializeField] private float runSpeed = 900f;
+
+    [SerializeField] private GameObject FollowCamera;
+
+
+
+    private void OnEnable()
+    {
+        if (inputActions == null) inputActions = new PlayerControlsButton();
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
 
     void Start()
     {
         currentSpeed = defaultSpeed;
 
+        TryGetComponent(out controller);
         TryGetComponent(out animator);
     }
 
@@ -27,8 +48,15 @@ public class PlayerMovement : MonoBehaviour
 
         if (hasControl)
         {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
-            transform.Translate(Vector3.forward * (currentSpeed * 0.01f) * Time.deltaTime);
+            //transform.localRotation = Quaternion.LookRotation(moveDirection);
+
+            //회전
+            float rotationAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+
+            //이동
+            controller.Move(transform.forward * (currentSpeed * 0.01f) * Time.fixedDeltaTime);
         }
         else
         {
@@ -38,19 +66,25 @@ public class PlayerMovement : MonoBehaviour
 
     public void onMove(InputAction.CallbackContext context)
     {
-
         Vector2 input = context.ReadValue<Vector2>();
+
         if (input != null)
         {
-            moveDirection = new Vector3(input.x, 0f, input.y);
-            Debug.Log("유니티 이벤트 : " + input.magnitude);
+            // 카메라의 전방 벡터를 획득
+            Vector3 cameraForward = Vector3.Scale(FollowCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+            Debug.Log(cameraForward);
+            // 입력값과 카메라의 전방 벡터를 조합하여 이동 방향 계산
+            moveDirection = (input.x * FollowCamera.transform.right + input.y * cameraForward).normalized;
+
+            //moveDirection = new Vector3(input.x, 0f, input.y);
+            //Debug.Log("유니티 이벤트 : " + input.magnitude);
         }
 
-        if (animator.GetFloat("Velocity") != 2)
+        if (animator.GetFloat("Velocity") != 1)
         {
-            animator.SetFloat("Velocity", 1);
+            animator.SetFloat("Velocity", 2);
         }
-
     }
 
     public void onJump(InputAction.CallbackContext context)
@@ -60,16 +94,19 @@ public class PlayerMovement : MonoBehaviour
 
         }
     }
-    public void onDash(InputAction.CallbackContext context)
+    public void onWalk(InputAction.CallbackContext context)
     {
+        //대쉬 버튼을 꾹 눌렀을때
         if (context.performed)
         {
-            currentSpeed = runSpeed;
-            animator.SetFloat("Velocity", 2);
+            currentSpeed = walkSpeed;
+            animator.SetFloat("Velocity", 1);
         }
+        //대쉬 버튼을 땠을때
         else if (context.canceled)
         {
             currentSpeed = defaultSpeed;
+            animator.SetFloat("Velocity", 2);
         }
     }
 }
